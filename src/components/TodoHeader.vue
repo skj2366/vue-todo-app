87043844a543356e29f053f6a951b14f1cf39f48
DongKeun1 <skj2366@naver.com>
2020-01-13 17:20:52 +0900

    first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+      <h1>TODO it!!</h1>
+  </header>
+</template>
+
+<script>
+    export default {
+        
+    };
+</script>
+
+<style scoped>
+    h1 {
+        color: #2F3852;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem        
+    }
+</style>
+<style>
+    body {
+        text-align: center;
+        background-color: #f6f6f6;
+    }
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
+    }
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <input type="text" v-model="newTodoItem" />
+      <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods : {
+        addTodo : function() {
+            if(this.newTodoItem !== '') {
+                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.clearItem();
+            }
+        },
+        clearItem : function() {
+            this.newTodoItem = '';
+        }
+    }
+};
+</script>
+
+<style scoped>
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
+</style>
+
+<style>
+</style>
